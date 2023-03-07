f919aafe51aea2d74043ab78db65568f0ae2ac92
Bruno Simachulu <94797356+Br28@users.noreply.github.com>
2023-03-07 08:24:55 +0200

    Completed loops lesson

---
+++ Loops/loop.dart
@@ -0,0 +1,25 @@
+void main() {
+  // While Loop
+  print('\n********************* While Loop ****************************\n');
+
+  int x = 0;
+  while (x < 5) {
+    print('Hello world');
+    x++;
+  }
+
+  // D0-while Loop
+  print('\n******************** Do-While Loop *****************************\n');
+
+  int y = 0;
+  do {
+    print('Hello world');
+    y++;
+  } while (y < 3);
+
+  print('\n********************** For Loop ***************************\n');
+
+  for (int z = 0; z < 3; z++) {
+    print('Hello world');
+  }
+}
--- Loops/whileloop.dart
+++
@@ -1,7 +0,0 @@
-void main() {
-  int x = 0;
-  while (x < 5) {
-    print('Hello world');
-    x++;
-  }
-}
